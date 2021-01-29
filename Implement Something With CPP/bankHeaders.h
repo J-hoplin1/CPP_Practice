6f1f895f7480ca15d2de10ffbca6849ac5047fa9
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-01-29 23:45:51 +0900

    Create bankHeaders.h

---
+++ Implement Something With CPP/bankHeaders.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __bank
+#define __bank
+
+
+
+void showOptions(void);
+void MakeAccount(void);
+void DepositMoney(void);
+void WithdrawMoney(void);
+void GetAllDatas(void);
+
+
+#endif
